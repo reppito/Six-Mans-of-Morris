da803f03b5ec456c7e1c354c5e7b04d46eeaadc6
reppito <repp0731@gmail.com>
2016-04-23 21:58:19 -0430

    Subiendo clase Consultas

---
+++ Six Men's Morris MVC/src/Modelo/Consultas.java
@@ -0,0 +1,52 @@
+package Modelo;
+
+import org.jpl7.Query;
+import org.jpl7.Term;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created by Raul on 4/23/2016.
+ */
+public class Consultas {
+    ListaPosiciones lista;
+    Query q1;
+    public Consultas(){
+        lista= new ListaPosiciones();
+        q1= new Query("consult('SMH.pl')");
+        q1.oneSolution();
+    }
+    public boolean tresEnLinea(int i)
+    {
+
+
+        if(i==0||i==1||i==2&&(lista.getElementInt(0)==lista.getElementInt(1)&&lista.getElementInt(2)==lista.getElementInt(0)) )
+            return true;
+        else if(i==0||i==6||i==13&&(lista.getElementInt(0)==lista.getElementInt(6)&&lista.getElementInt(13)==lista.getElementInt(0)))
+          return true;
+        else if(i==13||i==14||i==15&&(lista.getElementInt(13)==lista.getElementInt(14)&&lista.getElementInt(14)==lista.getElementInt(15)))
+            return true;
+        else if(i==2||i==9||i==15&&(lista.getElementInt(2)==lista.getElementInt(9)&&lista.getElementInt(9)==lista.getElementInt(15)))
+            return true;
+        else if(i==3||i==4||i==5&&(lista.getElementInt(3)==lista.getElementInt(4)&&lista.getElementInt(4)==lista.getElementInt(5)))
+            return true;
+        else if(i==3||i==7||i==10&&(lista.getElementInt(3)==lista.getElementInt(7)&&lista.getElementInt(7)==lista.getElementInt(10)))
+            return true;
+        else if(i==10||i==11||i==12&&(lista.getElementInt(10)==lista.getElementInt(11)&&lista.getElementInt(11)==lista.getElementInt(12)))
+            return true;
+        else if(i==5||i==8||i==12&&(lista.getElementInt(5)==lista.getElementInt(8)&&lista.getElementInt(8)==lista.getElementInt(12)))
+            return true;
+        else return false;
+
+    }
+    public ArrayList<Integer> movimientos(int i)
+    {  System.out.println(lista.getElementString(i));
+        ArrayList<Integer> pos= new ArrayList<Integer>();
+        Query movimientos= new Query("verificarMovimientos("+lista.toStringInt()+","+lista.getElementString(i)+",X)");
+        Map<String, Term>[] soluciones= movimientos.allSolutions();
+        for(int j=0;j<soluciones.length;j++)
+            pos.add(Integer.parseInt(soluciones[j].get("X").toString()));
+        return pos;
+    }
+}
